99466d3a228fd021e0720b6557b0df56148664cd
goddices <qq470857673@gmail.com>
2019-01-31 10:54:19 +0800

    left code rewrite

---
+++ WebHost/Controllers/TempController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebHost.Controllers
+{
+    public class TempController : ApiController
+    {
+        public struct RequestInfo
+        {
+
+            public string RequestId { get; set; }
+            public int ActionThreadId { get; set; }
+            public int TaskThreadId { get; set; }
+        }
+        public async Task<RequestInfo> Get()
+        {
+            RequestInfo ri = new RequestInfo
+            {
+                ActionThreadId = Thread.CurrentThread.ManagedThreadId,
+                RequestId = Guid.NewGuid().ToString()
+            };
+
+            //
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000); ri.TaskThreadId = Thread.CurrentThread.ManagedThreadId;
+            });
+            Thread.Sleep(4000);
+            return ri;
+        }
+    }
+}
