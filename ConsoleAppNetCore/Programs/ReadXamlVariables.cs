0738eee16e8e0c2233e08f63e3e6bcdabf5ce6b3
goddi <goddi@DESKTOP-OMOBU7U>
2020-07-07 13:24:57 +0800

    file

--- ConsoleAppNetCore/Programs/ReadXamlVariables.cs
+++ ConsoleAppNetCore/Programs/ReadXamlVariables.cs
@@ -14,13 +14,13 @@ namespace ConsoleAppNetCore.Programs
     public class ReadXamlVariables
     {
 
-        public async Task<(bool Valid, IEnumerable<object> Arguments)> TryReadAsync(Stream dgsFileStream, CancellationToken token)
-        {
-            XmlReader reader = XmlReader.Create(dgsFileStream);
-            while (await reader.ReadAsync() && reader.NodeType == XmlNodeType.Element && string.Equals("x:Members", reader.Name))
-            {
-                break;
-            }
-        }
+        //public async Task<(bool Valid, IEnumerable<object> Arguments)> TryReadAsync(Stream dgsFileStream, CancellationToken token)
+        //{
+        //    XmlReader reader = XmlReader.Create(dgsFileStream);
+        //    while (await reader.ReadAsync() && reader.NodeType == XmlNodeType.Element && string.Equals("x:Members", reader.Name))
+        //    {
+        //        break;
+        //    }
+        //}
     }
 }
