f6d49271177233442bd590cb935ac0da86112d04
goddi <goddi@DESKTOP-OMOBU7U>
2020-08-24 22:54:11 +0800

    locataliztion

---
+++ ResourcesTests/ResourceWebTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace ResourceWebTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IStringLocalizer<WeatherForecastController> _localizer;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            IStringLocalizer<WeatherForecastController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _localizer.GetString("String1").Value;
+        }
+    }
+}
